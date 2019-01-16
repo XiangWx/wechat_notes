2c03851b18746c16070026c2eee118c8e6fb9e2b
XiangWx <1013109374@qq.com>
2019-01-16 10:06:30 +0800

    微信公众号文档整理

---
+++ config.js
@@ -0,0 +1,13 @@
+self.$config = {
+    nav: [
+      {title: '概述', path: '/'},
+      {title: '接口说明', path: '/interface'},
+      {title: '聊天机器人', path: '/chatrobot'},
+      {title: '微信网页开发', path: '/page'},
+      {title: 'JS-SDK列表', path: '/jssdklist'},
+      {title: 'access_token说明', path: '/accesstoken'},
+      {title: '附录', path: '/appendix'},
+      {title: 'ngrok使用', path: '/ngrok'}
+    ],
+    tocVisibleDepth: 2
+  }
